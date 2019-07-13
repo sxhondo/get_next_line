a4eb64462557bbf8b4ad232fb8e79ef9a87d2699
sxhondo <w13cho@gmail.com>
2019-07-13 17:59:54 +0300

    tabs fixed

--- get_next_line.c
+++ get_next_line.c
@@ -14,68 +14,68 @@
 
 t_list		*find_struct(t_list **file, int fd)
 {
-		t_list	*tmp;
+	t_list	*tmp;
 
-		tmp = *file;
-		while (tmp)
-		{
-			if (tmp->content_size == (size_t)fd)
-				if (tmp->content)
-					return (tmp);
-			tmp = tmp->next;
-		}
-		tmp = ft_lstnew("", 1);
-		tmp->content_size = (size_t)fd;
-		ft_lstadd(file, tmp);
-		return (tmp);
+	tmp = *file;
+	while (tmp)
+	{
+		if (tmp->content_size == (size_t)fd)
+			if (tmp->content)
+				return (tmp);
+		tmp = tmp->next;
+	}
+	tmp = ft_lstnew("", 1);
+	tmp->content_size = (size_t)fd;
+	ft_lstadd(file, tmp);
+	return (tmp);
 }
 
 int 	convert(t_list **file, char **line)
 {
-		int 	i;
-		t_list	*tmp;
-		char	*buf;
-		char	*left;
+	int 	i;
+	t_list	*tmp;
+	char	*buf;
+	char	*left;
 
-		i = 0;
-		tmp = *file;
-		buf = (char *)tmp->content;
-		while (buf[i] && buf[i] != '\n')
-			i++;
-		*line = ft_strndup(buf, i);
-		left = (ft_strchr(buf, '\n') + 1);
-		tmp->content = left;
-		return (1);
+	i = 0;
+	tmp = *file;
+	buf = (char *)tmp->content;
+	while (buf[i] && buf[i] != '\n')
+		i++;
+	*line = ft_strndup(buf, i);
+	left = (ft_strchr(buf, '\n') + 1);
+	tmp->content = left;
+	return (1);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-		static t_list	*file;
-		int				bytes;
-		char			buf[BUFF_SIZE + 1];
+	static t_list	*file;
+	int				bytes;
+	char			buf[BUFF_SIZE + 1];
 
-		if (fd < 0 || !line || BUFF_SIZE < 1 || read(fd, buf, 0) < 0)
-			return (-1);
+	if (fd < 0 || !line || BUFF_SIZE < 1 || read(fd, buf, 0) < 0)
+		return (-1);
 
-		file = find_struct(&file, fd);
-		// printf("FILE->CONTENT: %s\n", file->content);
-		while ((bytes = read(fd, buf, BUFF_SIZE)))
-		{
-			buf[bytes] = '\0';
-			file->content = ft_strjoin(file->content, buf);
-			if (ft_strchr(file->content, '\n'))
-				break;
-		}
-		if (!bytes)
-			return (0);
-		if (bytes < BUFF_SIZE)
-		{
-			*line = ft_strdup(file->content);
-			return (1);
-		}
-		else
-			return (convert(&file, line));
+	file = find_struct(&file, fd);
+	// printf("FILE->CONTENT: %s\n", file->content);
+	while ((bytes = read(fd, buf, BUFF_SIZE)))
+	{
+		buf[bytes] = '\0';
+		file->content = ft_strjoin(file->content, buf);
+		if (ft_strchr(file->content, '\n'))
+			break;
+	}
+	if (!bytes)
 		return (0);
+	if (bytes < BUFF_SIZE)
+	{
+		*line = ft_strdup(file->content);
+		return (1);
+	}
+	else
+		return (convert(&file, line));
+	return (0);
 }
 
 // int		main()

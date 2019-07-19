b61f1e99c0104d049933adfefa9617f05abb4d76
sxhondo <sxhondo@student.42.fr>
2019-07-19 17:33:31 +0300

    working great

--- main.c
+++ main.c
@@ -1,37 +1,15 @@
-
 #include "get_next_line.h"
-#include <stdio.h>
 
-int 		main(int argc, char **argv)
+int 	main(int argc, char **argv)
 {
+	char 	*line;
 	int 	fd;
-	int 	ret;
-	int 	line;
-	char 	*buff;
 
-	line = 0;
-	if (argc == 2)
+	fd = open(argv[1], O_RDONLY);
+	while (get_next_line(fd, &line) > 0)
 	{
-		fd = open(argv[1], O_RDONLY);
-		while ((ret = get_next_line(fd, &buff)) > 0) {
-			printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
-			free(buff);
-		}
+		ft_putstr(line);
+		ft_putchar('\n');
+		free (line);
 	}
-       printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
-       if (ret == -1)
-           printf("-----------\nError\n");
-       else if (ret == 0)
-           printf("-----------\nEnd of file\n");
-       close(fd);
-   if (argc == 1)
-   {
-       while ((ret = get_next_line(0, &buff)) > 0)
-           printf("[Return: %d] Line #%d: %s\n", ret, ++line, buff);
-       if (ret == -1)
-           printf("-----------\nError\n");
-       else if (ret == 0)
-           printf("-----------\nEnd of stdin\n");
-       close(fd);
-   }
 }
